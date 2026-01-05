6b92cbcb7cb1ccfc5079587588a80ad3c7fc6049
endyzan5 <andizainuri5@gmail.com>
2026-01-05 13:50:16 +0700

    2

--- includes/header.php
+++ includes/header.php
@@ -5,8 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desa Mandiri Jaya - Profil Desa</title>
     <link href="https://fonts.googleapis.com/css2?family=Fraunces:wght@300;600;900&family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <link rel="stylesheet" href="../assets/js/scripts.js">
-
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/js/scripts.js">
 </head>
 <body>
